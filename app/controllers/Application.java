4e645c0476b584faa413dea128fb186b7209f34b
Gordon <gb@gordonbailey.net>
2015-08-17 19:14:39 -0400

    Added basic schedule page which should work for any employee

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,10 +1,14 @@
 package controllers;
 
+import java.util.List;
+
+import model.Schedule;
 import model.SessionAuth;
 import model.SessionAuth.SessionInfo;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.schedule;
 
 public class Application extends Controller {
 	
@@ -17,5 +21,18 @@ public class Application extends Controller {
 			return redirect(controllers.routes.LogInOut.login());
 		}
 	}
+	
+	public static Result viewSchedule(){		
+		SessionInfo sess = SessionAuth.instance.getSession(session());
+
+		if(sess != null){
+			List<Schedule> s = Schedule.getSchedule(sess.employeeId);
+			
+			return ok(schedule.render(s));			
+		}else{
+			return redirect(controllers.routes.LogInOut.login());
+		}
+	}
+
 
 }
