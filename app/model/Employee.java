06ec37c3ed594e8eacc89d21821f08f454dac138
Patrick Nguyen <Patrick.Nguyen@mindgeek.com>
2015-08-19 14:26:45 -0400

    Add getPayrolls to the Employee model
    This method will get ALL of the Payrolls for a specific employee, not
    just the upcoming one.

--- app/model/Employee.java
+++ app/model/Employee.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Base class for all employees.
  * 
@@ -16,6 +23,42 @@ public class Employee {
 
 	public Employee(int employeeId) {
 		this.employeeId = employeeId;
+	}
+
+	// Get all payroll statements for this employee
+	public static List<Payroll> getPayrolls() {
+		List<Payroll> payrolls = new LinkedList<Payroll>();
+		
+		try{
+			PreparedStatement s = DatabaseManager.instance.createPreparedStatement(
+					"SELECT payroll_id, title, pay_type, base_rate, seniority_bonus_amount, seniority_bonus_multiplier, overtime_multiplier " + 
+					"FROM Payroll NATURAL JOIN Employee " + 
+					"WHERE Payroll." + payroll_id + "=Employee." + payroll_id);
+	
+			ResultSet r = null;
+			
+			try{
+				r = s.executeQuery();
+				
+				while(r.next()){
+					payrolls.add(new Payroll(
+						r.getInt("payroll_id"), 
+						r.getString("title"), 
+						Pay_Type.valueOf(r.getString("payType")), // not sure if we can do this for enums... 
+						r.getDouble("base_rate"),
+						r.getInt("seniority_bonus_amount"),
+						r.getFloat("seniority_bonus_multiplier"),
+						r.getFloat("overtime_multiplier")));
+				}
+				
+			}finally{
+				if (r != null) r.close();
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return payrolls;
 	}
 
 	public int getEmployeeId() {
